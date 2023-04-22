4d29c46b752cb30257897350b694797f733d0fba
송호준 <sson514@wemakeprice.com>
2023-04-22 15:31:37 +0900

    2023-03-22 sol BJ 1

---
+++ workspace/BJ_1032.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+cmdList = [input() for _ in range (N)]
+
+result = ""
+for i in range (len(cmdList[0])):
+    tmp = cmdList[0][i]
+    for j in range (1, N):
+        if (cmdList[j][i] != tmp): tmp = '?'
+
+    result += tmp
+
+print(result)
